6c15223af4a1c08029ebdc633f991442b70de4e5
Rob Warner <rob.warner@availity.com>
2018-05-16 10:50:05 -0400

    Create CSV

--- README.md
+++ README.md
@@ -1,6 +1,29 @@
 # Jags
 
-> Scrapes the Jaguars' roster from the official website
+> Scrapes the Jaguars' roster from the official website and creates a CSV
+
+## Installation
+
+```sh
+$ go get github.com/hoop33/jags
+```
+
+## Usage
+
+```sh
+$ jags
+```
+
+## Credits
+
+* [goquery](https://github.com/PuerkitoBio/goquery)
+
+## Disclaimers
+
+* It works as of May 16, 2018
+* It scrapes <https://www.jaguars.com/team/players-roster/>
+* It's vulnerable to a MITM attack (TLS verification is turned off) because reasons
+* Fields aren't surrounded by quotes, so a comma in a value could throw things off (I could surround with quotes, and then escape embedded quotes, and then . . .)
 
 ## License
 
--- main.go
+++ main.go
@@ -1,7 +1,40 @@
 package main
 
-import "fmt"
+import (
+	"crypto/tls"
+	"fmt"
+	"log"
+	"net/http"
+	"strings"
+
+	"github.com/PuerkitoBio/goquery"
+)
 
 func main() {
-	fmt.Println("vim-go")
+	http.DefaultTransport.(*http.Transport).TLSClientConfig = &tls.Config{InsecureSkipVerify: true}
+	resp, err := http.Get("https://www.jaguars.com/team/players-roster/")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer resp.Body.Close()
+	if resp.StatusCode != 200 {
+		log.Fatalf("Failed to get roster from team site: %d %s", resp.StatusCode, resp.Status)
+	}
+
+	doc, err := goquery.NewDocumentFromReader(resp.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	doc.Find("[summary='Roster']").Find("tbody tr").Each(func(i int, s *goquery.Selection) {
+		name, exists := s.Find("a").Attr("title")
+		if exists {
+			fmt.Printf("%s", name)
+			for i := 1; i <= 7; i++ {
+				fmt.Printf(",%s", strings.TrimSpace(s.Find("td").Eq(i).Text()))
+			}
+			fmt.Println()
+		}
+	})
 }
